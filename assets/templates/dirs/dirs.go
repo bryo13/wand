5b4059d914136d12dd14bdfcfde92af84347007e
Brian Tum <brian.tum10@yahoo.com>
2023-11-08 12:01:16 +0300

    minimum viable product

--- assets/templates/dirs/dirs.go
+++ assets/templates/dirs/dirs.go
@@ -1,14 +1,34 @@
 // creates the directory structure
 package dirs
 
+import (
+	"fmt"
+	"os"
+)
+
 var (
 	mini = []string{"cmd", "pkg"}
 )
 
-func CreateParentDir(name string) error {
+func CreateHomeDir(projectName string) error {
+	err := os.Mkdir(projectName, 0750)
+	if err != nil {
+		fmt.Println(fmt.Errorf("could not create the directories due to: %q", err))
+		return err
+	}
+
 	return nil
 }
 
-func createChilddir() error {
+func CreateMiniDirs(projectName string) error {
+
+	for _, name := range mini {
+		name = fmt.Sprintf("%s/%s", projectName, name)
+		err := os.Mkdir(name, 0750)
+		if err != nil {
+			fmt.Println(fmt.Errorf("could not create the directories due to: %q", err))
+			return err
+		}
+	}
 	return nil
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
 
-	"github.com/bryo13/wand/cmd/wand"
+	"github.com/bryo13/wand/assets/templates/dirs"
 )
 
-func main() {
-	cmdArgs := os.Args[1]
-	if cmdArgs == "bin" {
-		wand.Cram()
-	}
+var (
+	mini        int
+	projectName string
+)
 
+func init() {
+	fmt.Println("Welcome to wand!")
+	fmt.Println("Checking if go is installed")
+	if !isGoInstalled() {
+		fmt.Println("please install Go from 'https://go.dev/doc/install'")
+		os.Exit(1)
+	}
+}
+
+func isGoInstalled() bool {
+	// 1. we can look for go in /usr/local directory but that limits us to linux
+	// 2. we can run go commands using exec to check if go is installed
+	// 3. the safer option is to use the LookPath function in the exec package
+
+	path, err := exec.LookPath("go")
+	if err != nil {
+		return false
+	}
+	fmt.Println("Go lives in ", path)
+	return true
+}
+func main() {
+
+	fmt.Print("Project name ")
+	fmt.Scanf("%s", &projectName)
+
+	fmt.Print("Choose dir type 1: mini ")
+	fmt.Scanf("%d", &mini)
+	if mini == 1 {
+		dirs.CreateHomeDir(projectName)
+		dirs.CreateMiniDirs(projectName)
+	}
 }
